24577729f08028a49e4e3f95399175e0a763fdc9
suryaprakash <singhyash2207@gmail.comm>
2021-08-21 19:26:00 +0530

    Initial commit: add file

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double math = 97.5;
+        double science = 82;
+        double english = 98;
+        double history = 79.4;
+        double sum = math+science+english+history;
+        double avarage = sum/4;
+        System.out.println(avarage);
+    }
+}
